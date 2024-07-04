481819da49fc90b7b15b05dabf88134942afdca3
Chandler Abril <abrilchandler1@gmail.com>
2024-07-04 07:07:19 -0700

    Done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    num_list_even = [num for num in num_list if num % 2 == 0]
+    return num_list_even
 def make_exclamation(sentence_list):
-    pass
+    exclamation = "!"
+    exclamation_sentence_list = [sentence + exclamation for sentence in sentence_list if sentence]
+    return exclamation_sentence_list
